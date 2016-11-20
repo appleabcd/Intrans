6e5386e9a6745b53bd1b7db5e7e7f0f061f1795d
Sangadji Prabowo <sangadji@altrovis.com>
2016-11-21 02:30:18 +0700

    asda

--- Intrans/ViewController.swift
+++ Intrans/ViewController.swift
@@ -15,7 +15,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        Label1.text = "Halo dunia!"
         
         
         // Do any additional setup after loading the view, typically from a nib.
