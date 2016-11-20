b22fd4ea9afe33ec80daf4442def898c3234c501
Sangadji Prabowo <sangadji@altrovis.com>
2016-11-21 02:00:08 +0700

    Initial Commit

---
+++ Intrans/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Intrans
+//
+//  Created by Raufan Multahada Safari on 11/21/16.
+//  Copyright © 2016 JAYA REAL PROPERTY, PT TBK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
